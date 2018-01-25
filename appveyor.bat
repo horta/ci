24e87dbd14a2a6f28870361bc685ec5bc21900b8
Danilo Horta <danilo.horta@gmail.com>
2018-01-25 00:21:12 +0000

    Update appveyor.bat

--- appveyor.bat
+++ appveyor.bat
@@ -2,15 +2,15 @@
 
 set PATH=%PYTHON%;%PYTHON%\\Scripts;%PATH%
 pip install -U setuptools pip pypandoc
-python setup.py test && git clean -xdf
-pip install . && git clean -xdf
+python setup.py build_ext --inplace
+python setup.py test
+pip install .
 cd ..
 python -c "import sys; import %PKG_NAME%; sys.exit(%PKG_NAME%.test())"
 pip uninstall %PKG_NAME% --yes
-cd %APPVEYOR_BUILD_FOLDER% && git clean -xdf
+cd %APPVEYOR_BUILD_FOLDER%
 pip install -r requirements.txt
 cd doc && make html && cd ..
-git clean -xdf
 python setup.py sdist
 
 @echo on
