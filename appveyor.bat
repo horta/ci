83e6323325f3b69d7f1462501bd7bf81a3e71003
Danilo Horta <danilo.horta@gmail.com>
2018-01-25 00:30:39 +0000

    Update appveyor.bat

--- appveyor.bat
+++ appveyor.bat
@@ -2,15 +2,26 @@
 
 set PATH=%PYTHON%;%PYTHON%\\Scripts;%PATH%
 pip install -U setuptools pip pypandoc
+
 python setup.py build_ext --inplace
+if errorlevel 1 exit 1
+
 python setup.py test
+if errorlevel 1 exit 1
+
 pip install .
 cd ..
+
 python -c "import sys; import %PKG_NAME%; sys.exit(%PKG_NAME%.test())"
+if errorlevel 1 exit 1
+
 pip uninstall %PKG_NAME% --yes
 cd %APPVEYOR_BUILD_FOLDER%
 pip install -r requirements.txt
-cd doc && make html && cd ..
-python setup.py sdist
+
+cd doc && make html
+if errorlevel 1 exit 1
+
+cd .. && python setup.py sdist
 
 @echo on
