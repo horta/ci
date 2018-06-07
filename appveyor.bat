28b13f4cd4674b7cdc0ea046efbf8913cce223af
Danilo Horta <danilo.horta@gmail.com>
2018-06-07 17:25:35 +0100

    Update appveyor.bat

--- appveyor.bat
+++ appveyor.bat
@@ -1,7 +1,7 @@
 @echo off
 
 set PATH=%PYTHON%;%PYTHON%\\Scripts;%PATH%
-pip install -U setuptools pip pypandoc
+pip install -U setuptools pip pytest pytest-pycodestyle -q
 
 python setup.py build_ext --inplace
 if errorlevel 1 exit 1
