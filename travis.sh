b2e518e1d66d40f70e3ae51f86b2dfc4dfa6729a
Danilo Horta <danilo.horta@gmail.com>
2018-08-02 17:00:23 +0100

    Update travis.sh

--- travis.sh
+++ travis.sh
@@ -6,7 +6,7 @@ python -m pip install -U setuptools pip pytest pytest-pycodestyle -q
 python -m pip install -U numpy -q
 python setup.py test && git clean -xdf
 python -m pip install . && git clean -xdf
-cd ..
+cd /
 python -c "import sys; import $PKG_NAME; sys.exit($PKG_NAME.test())"
 python -m pip uninstall $PKG_NAME --yes
 cd $TRAVIS_BUILD_DIR && git clean -xdf
@@ -15,6 +15,6 @@ python -m pip install -r requirements.txt -q
 git clean -xdf
 python setup.py sdist
 python -m pip install dist/$(ls dist | grep -i -E '\.(gz)$' | head -1)
-cd ..
+cd /
 python -c "import sys; import $PKG_NAME; sys.exit($PKG_NAME.test())"
 cd $TRAVIS_BUILD_DIR
