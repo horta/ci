7771b81aaeaf45cde94cee60401b4b5acef90386
Danilo Horta <danilo.horta@gmail.com>
2018-08-17 11:22:38 +0100

    Update appveyor.bat

--- appveyor.bat
+++ appveyor.bat
@@ -1,20 +1,21 @@
 @echo off
 
 echo %PATH%
-python -m pip install -U cffi numpy pip pytest pytest-pycodestyle setuptools -q
+echo %PYTHON%
+%PYTHON% -m pip install -U cffi numpy pip pytest pytest-pycodestyle setuptools -q
 
-python setup.py test
+%PYTHON% setup.py test
 if errorlevel 1 exit 1
 
-python -m pip install .
+%PYTHON% -m pip install .
 cd ..
 
-python -c "import sys; import %PKG_NAME%; sys.exit(%PKG_NAME%.test())"
+%PYTHON% -c "import sys; import %PKG_NAME%; sys.exit(%PKG_NAME%.test())"
 if errorlevel 1 exit 1
 
-pip uninstall %PKG_NAME% --yes
+%PYTHON% -m pip uninstall %PKG_NAME% --yes
 cd %APPVEYOR_BUILD_FOLDER%
-pip install -r requirements.txt
+%PYTHON% -m pip install -r requirements.txt
 
 if exist doc\make.bat (
   cd doc && make.bat html
@@ -22,6 +23,6 @@ if exist doc\make.bat (
   cd %APPVEYOR_BUILD_FOLDER%
 )
 
-python setup.py sdist
+%PYTHON% setup.py sdist
 
 @echo on
