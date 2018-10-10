79f2efe096d30670c5288aa7c139dd56bff47d8b
Danilo Horta <danilo.horta@gmail.com>
2018-10-10 09:28:16 +0100

    {

--- travis-c.sh
+++ travis-c.sh
@@ -10,10 +10,10 @@ cmake .. -DCMAKE_INSTALL_PREFIX=$TRAVIS_BUILD_DIR -DCMAKE_BUILD_TYPE=Release
 make && make test && make install
 popd && rm -rf build
 if [[ "$TRAVIS_OS_NAME" == "linux" ]]; then test -e $TRAVIS_BUILD_DIR/lib/lib$PKG_NAME.so; fi
-if [[ "$TRAVIS_OS_NAME" == "linux" ]]; then test -e $TRAVIS_BUILD_DIR/lib/lib$PKG_NAME_static.a; fi
+if [[ "$TRAVIS_OS_NAME" == "linux" ]]; then test -e $TRAVIS_BUILD_DIR/lib/lib${PKG_NAME}_static.a; fi
 if [[ "$TRAVIS_OS_NAME" == "linux" ]]; then test -e $TRAVIS_BUILD_DIR/include/$PKG_NAME.h; fi
 if [[ "$TRAVIS_OS_NAME" == "osx" ]]; then test -e $TRAVIS_BUILD_DIR/lib/lib$PKG_NAME.dylib; fi
-if [[ "$TRAVIS_OS_NAME" == "osx" ]]; then test -e $TRAVIS_BUILD_DIR/lib/lib$PKG_NAME_static.a; fi
+if [[ "$TRAVIS_OS_NAME" == "osx" ]]; then test -e $TRAVIS_BUILD_DIR/lib/lib${PKG_NAME}_static.a; fi
 if [[ "$TRAVIS_OS_NAME" == "osx" ]]; then test -e $TRAVIS_BUILD_DIR/include/$PKG_NAME.h; fi
 bash <(curl -fsSL https://raw.githubusercontent.com/limix/$PKG_NAME/master/install)
 
