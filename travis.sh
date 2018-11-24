8bce01a1d8a7eee707c4e93d63b84e6d365707b3
Danilo Horta <danilo.horta@gmail.com>
2018-11-24 18:50:53 +0000

    black check

--- travis.sh
+++ travis.sh
@@ -3,7 +3,23 @@
 set -e
 
 python -m pip install -U setuptools pip pytest pytest-pycodestyle -q
-python -m pip install -U numpy -q
+python -m pip install -U numpy black flake8 doc8 -q
+
+# Make sure the code has been formatted.
+git clean -xdf
+find . -type f -name "*.py" -exec cksum "{}" \; | sort > checksum0.txt
+find . -type f -name "*.py" -exec black --quiet --fast {} \;
+find . -type f -name "*.py" -exec cksum "{}" \; | sort > checksum1.txt
+if ! diff checksum0.txt checksum1.txt >/dev/null;
+then
+    (>&2 echo "Please, apply the black Python code formatter on the following files:")
+    diff checksum0.txt checksum1.txt | sed '1d; n; d' | awk -F ' ' '{print $4}'
+    exit 1
+else
+    rm checksum0.txt
+    rm checksum1.txt
+fi
+
 python setup.py test && git clean -xdf
 python -m pip install --process-dependency-links . && git clean -xdf
 cd ~/
