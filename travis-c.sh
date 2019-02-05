1dd57e6d1ac2d1ce63a5e0e03d9083b5d593b9d0
Danilo Horta <danilo.horta@gmail.com>
2019-02-05 10:57:13 +0000

    install prefix

--- travis-c.sh
+++ travis-c.sh
@@ -17,14 +17,20 @@ function finish {
 trap finish EXIT
 
 function testit {
+    orig_dir=$(pwd)
     [ -z "$PKG_NAME" ] && PKG_NAME=$(cat NAME)
 
     bash <(curl -fsSL https://raw.githubusercontent.com/horta/ci/master/check-version.sh)
 
     mkdir build 2>/dev/null || true
     pushd build
-    [ -z "$TRAVIS_BUILD_DIR" ] && BUILD_DIR=$(mktemp -d) || BUILD_DIR=$TRAVIS_BUILD_DIR
-    cmake .. -DCMAKE_INSTALL_PREFIX=$BUILD_DIR -DCMAKE_BUILD_TYPE=Release
+    if [ -z "$TRAVIS_BUILD_DIR" ]
+    then
+        IPREF=$(mktemp -d)
+    else
+        IPREF=$TRAVIS_BUILD_DIR/build/install
+    fi
+    cmake .. -DCMAKE_INSTALL_PREFIX=$IPREF -DCMAKE_BUILD_TYPE=Release
     make && make test && make install
     popd && rm -rf build
 
@@ -37,15 +43,15 @@ function testit {
         OS_NAME=$TRAVIS_OS_NAME
     fi
 
-    if [[ "$OS_NAME" == "linux" ]]; then test -e $BUILD_DIR/lib/lib$PKG_NAME.so; fi
-    if [[ "$OS_NAME" == "linux" ]]; then test -e $BUILD_DIR/lib/lib${PKG_NAME}_static.a; fi
-    if [[ "$OS_NAME" == "linux" ]]; then test -e $BUILD_DIR/include/$PKG_NAME.h; fi
-    if [[ "$OS_NAME" == "osx" ]]; then test -e $BUILD_DIR/lib/lib$PKG_NAME.dylib; fi
-    if [[ "$OS_NAME" == "osx" ]]; then test -e $BUILD_DIR/lib/lib${PKG_NAME}_static.a; fi
-    if [[ "$OS_NAME" == "osx" ]]; then test -e $BUILD_DIR/include/$PKG_NAME.h; fi
+    if [[ "$OS_NAME" == "linux" ]]; then test -e $IPREF/lib/lib$PKG_NAME.so; fi
+    if [[ "$OS_NAME" == "linux" ]]; then test -e $IPREF/lib/lib${PKG_NAME}_static.a; fi
+    if [[ "$OS_NAME" == "linux" ]]; then test -e $IPREF/include/$PKG_NAME.h; fi
+    if [[ "$OS_NAME" == "osx" ]]; then test -e $IPREF/lib/lib$PKG_NAME.dylib; fi
+    if [[ "$OS_NAME" == "osx" ]]; then test -e $IPREF/lib/lib${PKG_NAME}_static.a; fi
+    if [[ "$OS_NAME" == "osx" ]]; then test -e $IPREF/include/$PKG_NAME.h; fi
     bash <(curl -fsSL https://raw.githubusercontent.com/limix/$PKG_NAME/master/install)
 
-    cd $BUILD_DIR
+    cd $orig_dir
 }
 
 (set -x; testit)
