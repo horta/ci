74f476c7d804556283cddbf554ad83c6b08fffd5
Danilo Horta <danilo.horta@pm.me>
2019-07-13 20:29:51 +0100

    not quiet

--- travis-py-min-deps.sh
+++ travis-py-min-deps.sh
@@ -7,18 +7,18 @@ function has_conda() {
 }
 
 function install_deps() {
-    python -m pip install -U setuptools pip pytest pytest-pycodestyle -q
-    python -m pip install -U flake8 rstcheck pygments docutils -q
-    python -m pip install -U rstcheck -q
-    python -m pip install -U shell-timeit -q
+    python -m pip install -U setuptools pip pytest pytest-pycodestyle
+    python -m pip install -U flake8 rstcheck pygments docutils
+    python -m pip install -U rstcheck
+    python -m pip install -U shell-timeit
 
     # Try to install conda-provided numpy and scipy first as they
     # are faster because of the MKL library.
     if has_conda;
     then
-        conda install numpy scipy --yes -q
+        conda install numpy scipy --yes
     else
-        python -m pip install numpy scipy -q -U
+        python -m pip install numpy scipy -U
     fi
 }
 
