29a569bf980fe33ef6cab257df671b857aa23444
Danilo Horta <danilo.horta@pm.me>
2020-01-08 13:20:31 +0000

    check pytest install

--- py-dev-test.sh
+++ py-dev-test.sh
@@ -3,6 +3,11 @@
 function install_deps
 {
     
+    if type pytest >/dev/null 2>&1
+    then
+        return 0
+    fi
+
     if ! python3 -m pip install -U pytest
     then
         (>&2 echo "🔥 We have failed to install `pytest`.")
